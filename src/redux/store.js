654840f05fa10a1b1966081c7fd61bc9e2682c8d
Ankit Pandey <ankit221601@gmail.com>
2025-05-09 02:22:11 +0530

    comment out

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import imageReducer from "./imgSlice";
 
+// making store for state
 export const store = configureStore({
+  // reducer is like fn
+  // which handles events
   reducer: {
     images: imageReducer,
   },
