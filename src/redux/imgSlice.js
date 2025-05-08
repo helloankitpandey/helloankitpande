ddd4e18c34ef37e6ab11eab712a7fc14b5260496
Ankit Pandey <ankit221601@gmail.com>
2025-05-09 02:21:58 +0530

    make updates

--- src/redux/imgSlice.js
+++ src/redux/imgSlice.js
@@ -1,21 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+ import { createSlice } from "@reduxjs/toolkit";
 
+// intial imageList -> null
 const initialState = {
   imageList: [],
 };
 
+// Now making slice of images
 const imageSlice = createSlice({
   name: "images",
   initialState,
+  // reducers of imageSlice
   reducers: {
     addImage: (state, action) => {
       state.imageList.push(action.payload);
     },
     clearImages: (state) => {
+      // deleting all images
       state.imageList = [];
     },
   },
 });
+
 
 export const { addImage, clearImages } = imageSlice.actions;
 export default imageSlice.reducer;
